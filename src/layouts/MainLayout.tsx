a3ae69a3c30537368329ff23f3f0d99d41a14bed
ken <krazalan@2damcreative.com>
2025-04-20 17:17:55 +0800

    feat: update Tailwind CSS integration and add postcss support

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
+
 const MainLayout = () => {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
